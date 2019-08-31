89439d9894b9484fccdb803ea7fbe6a9207bf2ee
Louis <shushanxingzhe@126.com>
2019-08-31 23:22:51 +0800

    简化代码

--- plotlyTry.py
+++ plotlyTry.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import cufflinks as cf
-import plotly.offline
-cf.go_offline()
-cf.set_config_file(offline=False, world_readable=True)
+
+cf.go_offline(connected=False)
 
 data = {
         'Team':['LA','NY','SF','HT'],
         'First Score': [100, 90, 65, 95],
-        'Second Score': [30, 45, 45, 56],
+        'Second Score': [30, 45, 68, 56],
         'Third Score': [52, 40, 80, 98],
         'Fourth Score': [86, 76, 50, 65]}
 
